185bf35606c9cfb0b131ba55b995316cf9decbdb
Hieu Le <trunghieult1807@gmail.com>
2024-10-24 22:33:39 +0700

    Fix coinsweeper, notpixel

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from dotenv import load_dotenv
 async def main():
     load_dotenv()
     await asyncio.gather(
-        *await create_tasks("tomarket"),
-        *await create_tasks("coinsweeper"),
-        *await create_tasks("okxracer"),
+        # *await create_tasks("tomarket"),
+        # *await create_tasks("coinsweeper"),
+        # *await create_tasks("okxracer"),
         *await create_tasks("notpixel"),
     )
 
